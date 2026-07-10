49839a6997cd8af234189b99054a42d37ba6911b
DreamTeaveler <1746959603@qq.com>
2026-07-10 19:31:52 +0800

    fix: _on_fullscreen_stable中强制16:9 resize，消除F11路径窗口化全屏卡死

--- app/pages/mirror_window.py
+++ app/pages/mirror_window.py
@@ -176,6 +176,10 @@ class MirrorWindow(QMainWindow):
 
     def _on_fullscreen_stable(self):
         self._resizing = False
+        if not self.isFullScreen():
+            w = self.width()
+            h = max(540, int(w / ASPECT_RATIO))
+            self.resize(w, h)
         self.resized.emit()
 
     def _force_resize_to_aspect(self):
